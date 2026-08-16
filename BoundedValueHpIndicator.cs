ee0d5889d799e569e8f97e97aea79e67a26f96a4
end-4 <97237370+end-4@users.noreply.github.com>
2026-08-16 22:07:51 +0700

    add hp hud indicator examples

---
+++ BoundedValueHpIndicator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using ThornClient.HUD;
+using ThornClient.Managers;
+
+namespace ThornExamples;
+
+public class BoundedValueHpIndicator : BoundedValueHudModule {
+    public override string[] Tags => ["health", "hit points", "blood", "fuel"];
+    public override Sprite Icon => AssetManager.Get<Sprite>(HudManager.BundleKey, "plus_thick");
+
+    // We supply a bound and display name in the constructor
+    public BoundedValueHpIndicator() : base("thornExamples.boundedValueHp", "Example Bounded-value HP",
+        "Shows current health", bound: 100, decimalPlaces: 0, displayName: "Health") {
+    }
+
+    public override void OnUpdate() {
+        var nm = NewMovement.Instance;
+        if (nm == null) return;
+
+        // Update the main value bar
+        Value = nm.hp;
+
+        // Bound reduction indicates a temporary decrease. We use it for showing hard damage
+        BoundReduction = nm.antiHp;
+    }
+}
---
+++ TextHpIndicator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using ThornClient.HUD;
+using ThornClient.Managers;
+
+namespace ThornExamples;
+
+// Subclassing TextHudModule...
+public class TextHpIndicator : TextHudModule {
+    // Search tags and icon like a normal Module...
+    public override string[] Tags => ["health", "hit points", "blood", "fuel"];
+    public override Sprite Icon => AssetManager.Get<Sprite>(HudManager.BundleKey, "plus_thick");
+
+    // Constructor. You can put setting initializations here...
+    public TextHpIndicator() : base("thornExamples.textHp", "Example text HP", "Shows current health") {
+    }
+
+    public override void OnUpdate() {
+        var nm = NewMovement.Instance;
+        if (nm == null) return;
+        Text = $"{nm.hp}/100";
+    }
+}
